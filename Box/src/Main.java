6fbd6e906d0e992cdd737649e68211eb0282ce1e
kubar64 <kuba.r.pawlowski@gmail.com>
2016-05-16 12:39:51 +0200

    Wstepny podzial klas

--- Box/src/Main.java
+++ Box/src/Main.java
@@ -1,11 +1,33 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class Main
-{
+public class Main implements KeyListener{
 
-	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
-		//test
+	public static void main(String[] args) {
+		
+		while(true)
+		{
+			
+		}
 	}
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		System.out.println("Pomidorek");
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+
+		System.out.println(e.getKeyCode());
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
---
+++ Box/src/Odczyt.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Odczyt {
+	public static void odczyt(String[] args){
+	String kod_kreskowy; 
+    Scanner odczyt = new Scanner(System.in); 
+    kod_kreskowy = odczyt.nextLine();
+	}
+}
